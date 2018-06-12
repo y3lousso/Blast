d96145fb63ef9b6bc8760cfbb8cc83812d7e9281
Thomas Faget <thomas.faget@hotmail.fr>
2018-06-12 10:48:16 -0400

    Update DoundSignature

--- BeatDetector/BeatDetector/SoundSignature.cs
+++ BeatDetector/BeatDetector/SoundSignature.cs
@@ -80,8 +80,8 @@ namespace BeatDetector
         public float[][] ES(FloatComplex[][] s, int[] indPartition, float[] valuesT, float[] valuesFS)
         {
             float[][] signature = new float[2][];
-            List<int> freqs = new List<int>();
-            List<int> values = new List<int>();
+            List<float> freqs = new List<float>();
+            List<float> values = new List<float>();
 
             for (int i = 0; i < nbBands; i++)
             {
@@ -89,13 +89,73 @@ namespace BeatDetector
                 int iMax = indPartition[i + 1];
 
                 // Band extraction :
-                float[][] band = new float[iMax-iMin][];
-                NMar
+                FloatComplex[][] band = new FloatComplex[iMax-iMin][];
+                // TODO
 
+                // Localisation of locales max
+                float[][] localesMax = getLocalesMaximum(band);
+
+                // threshold : mean + std
+                float threshold = Mean(localesMax[1]) + STD(localesMax[1]);
+
+                // Get frequencies upper than the threshold
+                for (int j = 0; j < localesMax[0].Length; j++)
+                {
+                    if (localesMax[1][j] > threshold)
+                    {
+                        freqs.Add(localesMax[0][j]);
+                        values.Add(localesMax[1][j]);
+                    }
+                }
             }
+
+            signature[0] = freqs.ToArray();
+            signature[1] = values.ToArray();
+            return signature;
+        }
+
+        /**
+         * Return the maximum of each column of the band
+         * The first column of the return matrix contains the indices (int),
+         * the second the values of the maximum
+         */
+        public float[][] getLocalesMaximum(FloatComplex[][] band)
+        {
+            int n = band[0].Length;
+            int sizeBand = band.Length;
+
+            float[][] localesMax = new float[2][];
+            localesMax[0] = new float[n];
+            localesMax[1] = new float[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int k = 0;
+                int indMax = 0;
+                float max = -1;
+                while (k < sizeBand)
+                {
+                    if (Abs(band[k][i]) > max)
+                    {
+                        indMax = k;
+                        max = Abs(band[k][i]);
+                    }
+
+                    k++;
+                }
+
+                localesMax[0][i] = indMax;
+                localesMax[1][i] = max;
+            }
+
+            return localesMax;
         }
 
 
+
+        /**
+         * Gabor transform (complex values)
+         */
         public FloatComplex[][] TGabor(float[] signal, int nbSamplesPerBar)
         {
             int n = signal.Length;
@@ -120,6 +180,9 @@ namespace BeatDetector
         }
         
         
+        /**
+         * FFT inplace
+         */
         public FloatComplex[] ComplexFFt(float[] signal)
         {
 
@@ -138,6 +201,9 @@ namespace BeatDetector
             return fftVector.ToArray();
         }
 
+        /**
+         * Return the indice of the first number x in data such as x<value
+         */
         public int MinIndice(float[] data, float value)
         {
             int n = data.Length;
@@ -161,12 +227,16 @@ namespace BeatDetector
             return indMin;
         }
 
+        /** Mean
+         */
         public float Mean(float[] data)
         {
             return data.Sum() / data.Length;
         }
 
-        public double STD(float[] data)
+        /** Standard deviation 1D
+         */
+        public float STD(float[] data)
         {
             double std = 0;
             if (data.Length > 0)
@@ -175,8 +245,16 @@ namespace BeatDetector
                 double sum = data.Sum(d => Math.Pow(d - avg, 2));
                 std = Math.Sqrt((sum) / (data.Count() - 1));
             }
-            return std;
+            return (float) std;
 
+        }
+
+        /**
+         * Complex abs : sqrt(x^2 + y^2)
+         */
+        public float Abs(FloatComplex complex)
+        {
+            return (float) Math.Sqrt(Math.Pow(complex.Real, 2) + Math.Pow(complex.Imag, 2));
         }
     }
 }
