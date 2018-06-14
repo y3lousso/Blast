76ef5e8c9b737e49e9c9c944455393f2dc664b43
Thomas Faget <thomas.faget@hotmail.fr>
2018-06-14 17:30:24 -0400

    Add FileManager for signature

---
+++ BeatDetector/BeatDetector/SoundSignatureFileManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BeatDetector
+{
+    public class SoundSignatureFileManager
+    {
+        public static void SaveSoundSignature(string filepath, List<List<bool>> signature)
+        {
+
+            if (File.Exists(filepath))
+            {
+                File.Delete(filepath);
+            }
+            FileStream fs = File.Create(filepath);
+            fs.Close();
+
+            using (var tw = new StreamWriter(filepath, true))
+            {
+                for (int i = 0; i < signature.Count; i++)
+                {
+                    tw.WriteLine(string.Join(",", signature[i]));
+                }
+            }
+
+        }
+
+        public static List<List<bool>> LoadSoundSignature(string filepath)
+        {
+
+            List<List<bool>> signature = new List<List<bool>>();
+            if (!File.Exists(filepath))
+            {
+                throw new IOException("The file " + filepath + " doesn't exist !");
+            }
+
+            string[] file = File.ReadAllLines(filepath);
+            foreach (string s in file)
+            {
+                    signature.Add(s.Split(',').Select(bool.Parse).ToList());
+            }
+
+            return signature;
+        }
+    }
+}
