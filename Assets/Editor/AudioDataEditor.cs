98a19a2ae46ec8079e5760e5535da22d2ee57904
y3lousso <y3lousso@gmail.com>
2018-06-18 12:12:13 -0400

    Pattern management

--- Assets/Editor/AudioDataEditor.cs
+++ Assets/Editor/AudioDataEditor.cs
@@ -5,6 +5,7 @@ using UnityEditor;
 using UnityEditorInternal;
 using System.IO;
 using System.Linq;
+using System;
 
 [CustomEditor(typeof(AudioData))]
 [CanEditMultipleObjects]
@@ -12,17 +13,22 @@ public class AudioDataEditor : Editor {
     
     SerializedProperty audioClip;
     SerializedProperty beatPerMinute;
+    SerializedProperty startingOffset;    
+
+    // Random
     SerializedProperty nbFrames;
-    SerializedProperty randomSeed;
-    SerializedProperty startingOffset;
-
-    SerializedProperty signatureFilePath;
-
+    SerializedProperty randomSeed;   
     SerializedProperty isNotEmptyChance;
     SerializedProperty isDoubleChance;
     SerializedProperty isTopChance;
     SerializedProperty isExtremChance;
-    SerializedProperty isSameColorChance;   
+    SerializedProperty isSameColorChance;
+
+    // With signature
+    SerializedProperty signatureFilePath;
+
+    //With signature and patterns
+    SerializedProperty patternManager;
 
     SerializedProperty showListCubes;
     SerializedProperty listCubes;
@@ -31,6 +37,8 @@ public class AudioDataEditor : Editor {
     SerializedProperty showListWalls;
     SerializedProperty listWalls;
     private ReorderableList listWallsReordorable;
+
+    #region Inspector
 
     private void OnEnable()
     {
@@ -47,6 +55,8 @@ public class AudioDataEditor : Editor {
         isSameColorChance = serializedObject.FindProperty("isSameColorChance");
 
         signatureFilePath = serializedObject.FindProperty("signatureFilePath");
+
+        patternManager = serializedObject.FindProperty("patternManager");
 
         showListCubes = serializedObject.FindProperty("showListCubes");        
         listCubes = serializedObject.FindProperty("listCubes");
@@ -121,18 +131,11 @@ public class AudioDataEditor : Editor {
 
         EditorGUILayout.PropertyField(audioClip);
         EditorGUILayout.PropertyField(beatPerMinute);
-        EditorGUILayout.PropertyField(nbFrames);       
-
-        EditorGUILayout.PropertyField(signatureFilePath);
-
-        if (GUILayout.Button("Generate from signature (filepath)"))
-        {
-            ((AudioData)target).listCubes = GenerateFromFile(signatureFilePath.stringValue);
-        }
-
-        EditorGUILayout.LabelField("From Randomizer", EditorStyles.boldLabel);
-        EditorGUILayout.PropertyField(randomSeed);
         EditorGUILayout.PropertyField(startingOffset);
+                    
+        
+        EditorGUILayout.PropertyField(nbFrames);
+        EditorGUILayout.PropertyField(randomSeed);
         EditorGUILayout.Slider(isNotEmptyChance, 0f, 1f);
         EditorGUILayout.Slider(isDoubleChance, 0f, 1f);
         EditorGUILayout.Slider(isTopChance, 0f, 1f);
@@ -142,6 +145,20 @@ public class AudioDataEditor : Editor {
         if (GUILayout.Button("Randomize"))
         {
             ((AudioData)target).listCubes = Randomizer();
+        }
+
+        EditorGUILayout.PropertyField(signatureFilePath);
+
+        if (GUILayout.Button("Generate from signature (filepath)"))
+        {
+            ((AudioData)target).listCubes = GenerateFromFile(signatureFilePath.stringValue);
+        }
+
+        EditorGUILayout.PropertyField(patternManager);
+
+        if (GUILayout.Button("Generate from signature and patterns"))
+        {
+            ((AudioData)target).listCubes = GenerateFromFileAndPatterns(signatureFilePath.stringValue);
         }
 
         EditorGUILayout.PropertyField(showListCubes);
@@ -161,9 +178,13 @@ public class AudioDataEditor : Editor {
         serializedObject.ApplyModifiedProperties();
     }
 
+    #endregion
+
+    #region ListBuildingLogic
+
     private List<TargetCubeData> Randomizer()
     {
-        Random.InitState(randomSeed.intValue);
+        UnityEngine.Random.InitState(randomSeed.intValue);
 
         List<TargetCubeData> targetCubesData = new List<TargetCubeData>();
 
@@ -210,7 +231,7 @@ public class AudioDataEditor : Editor {
                     TargetCubeData current2 = new TargetCubeData();
                     current2.Id = i;
 
-                    Random.InitState(i);
+                    UnityEngine.Random.InitState(i);
 
                     if (isExtrem)
                     {
@@ -267,47 +288,121 @@ public class AudioDataEditor : Editor {
     /// <returns></returns>
     private List<TargetCubeData> GenerateFromFile(string filePath)
     {
-        List<TargetCubeData> targetCubesData = new List<TargetCubeData>();
-        
+        // From 2D array of bool to Tuple<Index,Size>
         List<List<bool>> lines = ExtractBoolArrayFromFile(Application.dataPath +"/"+ filePath);
+        List<bool> bools = Convert2dBoolArrayTo1d(lines);
+        List<Tuple<int, int>> tuples = GenerateTupleFromBool(bools);
 
-        int i = 0;     
-        foreach (List<bool> line in lines)
+        // Generate random targetCubeData for each tuple
+        List<TargetCubeData> targetCubeDatas = new List<TargetCubeData>();
+
+        foreach(Tuple<int,int> t in tuples)
         {
-            foreach (bool b in line)
+            targetCubeDatas.AddRange(GenerateDefaultPattern(t));
+        }
+
+        return targetCubeDatas;
+    }
+
+    private List<TargetCubeData> GenerateFromFileAndPatterns(string filePath)
+    {
+        // From 2D array of bool to Tuple<Index,Size>
+        List<List<bool>> lines = ExtractBoolArrayFromFile(Application.dataPath + "/" + filePath);
+        List<bool> bools = Convert2dBoolArrayTo1d(lines);
+        List<Tuple<int, int>> tuples = GenerateTupleFromBool(bools);
+
+        // Generate random targetCubeData for each tuple
+        List<TargetCubeData> targetCubeDatas = new List<TargetCubeData>();
+       
+        foreach (Tuple<int, int> t in tuples)
+        {
+            //Debug.Log(t);
+            List<Pattern> patterns = ((AudioData)target).patternManager.listPatterns.FindAll(p => p.length == t.Item2);
+            Pattern pattern = null;
+
+            if (patterns.Count > 0)
             {
-                if (b == true)
+                pattern = patterns[UnityEngine.Random.Range(0, patterns.Count)];
+            }
+
+            if ( pattern != null)
+            {
+                for (int i = 0; i < pattern.targetCubeDatas.Count; i++)
                 {
-                    TargetCubeData cube = new TargetCubeData { Id = i, cubeColor = CubeColor.Blue, horizontalPosition = HorizontalPosition.Right, vecticalPosition = VerticalPosition.Bot, orientation = Orientation.Top };
-                    if (i % 2 == 0)
-                    {
-                        cube.orientation = Orientation.Bot;
-                    }
-                    targetCubesData.Add(cube);
+                    TargetCubeData targetCube = new TargetCubeData(pattern.targetCubeDatas[i]);
+                    targetCube.Id = i+ t.Item1;                    
+                    targetCubeDatas.Add(targetCube);
+                }  
+            }
+            else
+            {
+                targetCubeDatas.AddRange(GenerateDefaultPattern(t));
+            }            
+        }
+
+        return targetCubeDatas;
+    }   
+
+    // Example 
+    // Index : 1  5  10
+    // Size  : 3  1  2
+    private List<Tuple<int, int>> GenerateTupleFromBool(List<bool> bools)
+    {
+        List<Tuple<int, int>> listTuples = new List<Tuple<int, int>>();
+
+        int currentIndex = -1;
+        int currentSize = 0;
+
+        for (int i = 0; i < bools.Count; i++)
+        {
+            if (bools[i])
+            {
+                if(currentIndex == -1)
+                {
+                    currentIndex = i;
+                }
+                currentSize++;
+                if(i == bools.Count - 1)
+                {
+                    listTuples.Add(new Tuple<int, int>(currentIndex, currentSize));
                 }
             }
-            i++;
+            else
+            {
+                if (currentIndex != -1)
+                {
+                    listTuples.Add(new Tuple<int, int>(currentIndex, currentSize));
+                    currentIndex = -1;
+                    currentSize = 0;
+                }
+            }
         }
 
-        return targetCubesData;
+        return listTuples;
     }
 
-    private bool RandomBool(float chance)
+    private List<TargetCubeData> GenerateDefaultPattern(Tuple<int,int> tuple)
     {
-        float random = Random.Range(0f, 1f);
-        if (random <= chance)
+        List<TargetCubeData> targetCubeDatas = new List<TargetCubeData>();
+        for (int i = 0; i < tuple.Item2; i++)
         {
-            return true;
+            TargetCubeData cube = new TargetCubeData { Id = tuple.Item1 + i, cubeColor = CubeColor.Blue, horizontalPosition = HorizontalPosition.Right, vecticalPosition = VerticalPosition.Bot, orientation = Orientation.Top };
+            if ((tuple.Item1 + i) % 2 == 0)
+            {
+                cube.orientation = Orientation.Bot;
+            }
+            targetCubeDatas.Add(cube);                
         }
-        else
-        {
-            return false;
-        }
+        return targetCubeDatas;
     }
+
+    #endregion
+
+    #region Utils
 
     private int RandomEnum(int[] enums)
     {
-        return enums[Random.Range(0, enums.Length)];
+        return enums[UnityEngine.Random.Range(0, enums.Length)];
     }
 
     private List<List<bool>> ExtractBoolArrayFromFile(string filePath)
@@ -326,4 +421,37 @@ public class AudioDataEditor : Editor {
 
         return signature;
     }
+
+    private bool RandomBool(float chance)
+    {
+        float random = UnityEngine.Random.Range(0f, 1f);
+        if (random <= chance)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private List<bool> Convert2dBoolArrayTo1d(List<List<bool>> lines)
+    {
+        List<bool> outputs = new List<bool>();
+
+        foreach (List<bool> line in lines)
+        {
+            if (line.Any(b => b.Equals(true)))
+            {
+                outputs.Add(true);
+            }
+            else
+            {
+                outputs.Add(false);
+            }
+        }
+        return outputs;
+    }
+
+    #endregion
 }
--- Assets/Scripts/AudioData.cs
+++ Assets/Scripts/AudioData.cs
@@ -8,19 +8,22 @@ public class AudioData : ScriptableObject
     [Header("Inputs")]
     [SerializeField] public AudioClip audioClip;
     [SerializeField] public int beatPerMinute = 90;
-    [SerializeField] public int nbFrames;
-    [SerializeField] public int randomSeed;
     [SerializeField] public float startingOffset = 0f;
 
-    [Header("From data Signature")]
-    [SerializeField] public string signatureFilePath = "";
-
     [Header("From Randomizer")]
+    [SerializeField] public int nbFrames;
+    [SerializeField] public int randomSeed;
     [SerializeField] public float isNotEmptyChance = 0.5f;
     [SerializeField] public float isDoubleChance = 0.5f;
     [SerializeField] public float isTopChance = 0.5f;
     [SerializeField] public float isExtremChance = 0.5f;
     [SerializeField] public float isSameColorChance = 0.5f;
+
+    [Header("From data Signature")]
+    [SerializeField] public string signatureFilePath = "";
+
+    [Header("From data Signature with pattern")]
+    [SerializeField] public PatternManager patternManager;
 
     [Header("Data")]
     [SerializeField] public bool showListCubes = false;
